76264883b7099c11cbc580319b0f12d0f96c7ba3
Ibrahim El Ouadifi <60790882+IbrahimElOuadifi@users.noreply.github.com>
2021-03-29 15:42:15 +0100

    add router

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const userSchema = mongoose.Schema({
     //id: { type: String, required: true, unique: true },
     name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true, lowercase: true },
     password: { type: String, required: true },
     createAt: { type: Date, default: Date.now }
 });
