3c0aa98a7368fb384b9e97639124643c1f6d3940
Ibrahim El Ouadifi <60790882+IbrahimElOuadifi@users.noreply.github.com>
2021-03-29 15:46:33 +0100

    add router

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ app.use('/like', likes);
 app.use('/comment', comments);
 app.use('/save', saves);
 
-//const CONNECTION_URL = 'mongodb+srv://root:toor@mern-blog-cluster.9m1r0.mongodb.net/database?retryWrites=true&w=majority';
-const CONNECTION_URL = 'mongodb://localhost:27017/mern-blogdb?readPreference=primary&appname=MongoDB%20Compass&ssl=false';
+const CONNECTION_URL = 'mongodb+srv://root:toor@mern-blog-cluster.9m1r0.mongodb.net/database?retryWrites=true&w=majority';
+//const CONNECTION_URL = 'mongodb://localhost:27017/mern-blogdb?readPreference=primary&appname=MongoDB%20Compass&ssl=false';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
