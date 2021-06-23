3d3ab08c53781d203653424171ce0c293d79beb2
DESKTOP-1Q2386G\eloua <elouadifi.ibrahim@gmail.com>
2021-06-23 16:43:06 +0100

    --

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use('/comment', comments);
 app.use('/save', saves);
 
 const CONNECTION_URL = 'mongodb+srv://root:toor@mern-blog-cluster.9m1r0.mongodb.net/database?retryWrites=true&w=majority';
-//const CONNECTION_URL = 'mongodb://localhost:27017/mern-blogdb?readPreference=primary&appname=MongoDB%20Compass&ssl=false';
+// const CONNECTION_URL = 'mongodb://localhost:27017/mern-blogdb?readPreference=primary&appname=MongoDB%20Compass&ssl=false';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
