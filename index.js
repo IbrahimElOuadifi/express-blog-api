ac022c12895c0aa95e9d317a6454f01fafd367d5
IbrahimElOuadifi <60790882+IbrahimElOuadifi@users.noreply.github.com>
2023-02-01 14:40:06 +0100

    fix isuess

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import { auth, follows, posts, likes, comments, saves } from './routes/index.js';
+import { config } from 'dotenv';
+
+config();
 
 const app = express();
 
@@ -15,7 +18,7 @@ app.use('/like', likes);
 app.use('/comment', comments);
 app.use('/save', saves);
 
-const CONNECTION_URL = 'mongodb+srv://root:toor@mern-blog-cluster.9m1r0.mongodb.net/database?retryWrites=true&w=majority';
+const CONNECTION_URL = process.env.DATABASE_CONNECTION_URL;
 // const CONNECTION_URL = 'mongodb://localhost:27017/mern-blogdb?readPreference=primary&appname=MongoDB%20Compass&ssl=false';
 const PORT = process.env.PORT || 5000;
 
